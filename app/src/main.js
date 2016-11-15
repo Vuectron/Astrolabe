302df080084e70ec27f26822fc250796a1928052
xiaoluoboding <xiaoluoboding@gmail.com>
2016-11-16 00:47:54 +0800

    First commit

---
+++ app/src/main.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import Electron from 'vue-electron'
+import VueRouter from 'vue-router'
+import App from './App'
+import routes from './routes'
+
+Vue.use(Electron)
+Vue.use(VueRouter)
+Vue.config.debug = true
+
+const router = new VueRouter({
+  scrollBehavior: () => ({ y: 0 }),
+  routes
+})
+
+/* eslint-disable no-new */
+new Vue({
+  router,
+  ...App
+}).$mount('#app')
